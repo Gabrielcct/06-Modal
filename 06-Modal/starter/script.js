daaaf60ebb6c0fa8927eb3b49e60c54ad29f133d
Gabriel <2021240@student.cct.ie>
2023-02-01 17:32:41 +0000

    final part

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -4,7 +4,26 @@ const modal = document.querySelector(".modal");
 const overlay = document.querySelector(".overlay");
 const btnCloseModal = document.querySelector(".close-modal");
 const btnsOpenModal = document.querySelectorAll(".show-modal");
-console.log(btnsOpenModal);
+
+const openModal = function() {
+    console.log("Button clicked");
+    modal.classList.remove("hidden");
+    overlay.classList.remove("hidden");
+};
+
+const CloseModal = function() {
+    modal.classList.add("hidden");
+    overlay.classList.add("hidden");
+};
 
 for (let i = 0; i < btnsOpenModal.length; i++)
-    console.log(btnsOpenModal[i].textContent);
+    btnsOpenModal[i].addEventListener("click", openModal);
+
+btnCloseModal.addEventListener("click", CloseModal);
+overlay.addEventListener("click", CloseModal);
+
+document.addEventListener("keydown", function(e) {
+    if (e.key === "Escape" && !modal.classList.contains("hidden")) {
+        CloseModal();
+    }
+});
